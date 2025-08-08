86ab39a4f895cc70cf3a1c369d7ff4b9d57b12cf
jongmin-gim <jongmin-gim@users.noreply.github.com>
2025-08-08 01:25:07 +0000

    deploy: 4170ffbae95ba53c2229a50b7d59b6ea02e04eda

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.demo","l":"GradleBasedSpringBackendApplication"},{"p":"com.example.demo","l":"HelloController"}];updateSearchResults();
